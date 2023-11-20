dd7f5a6504782f49c5b240c5ea1414a2898d5bbe
Christopher Kearl <c2_kearl@yahoo.com>
2023-11-20 15:52:19 -0700

    font awesome icons bootstrap and one more package

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { useState } from 'react';
+import { library } from '@fortawesome/fontawesome-svg-core'
 import Home from './Home';
 import Grid from './Grid';
 import Toolbox from './Toolbox';
 import Settings from './Settings';
 import Navbar from './Navbar';
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { faFontAwesome } from '@fortawesome/free-brands-svg-icons'
+
+library.add(fas, faFontAwesome)
 
 const App = () => {
   const [currentPage, setCurrentPage] = useState("Home");
