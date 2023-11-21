1cfbbb96f9ebac0f446e85835beb836c37d4576b
corbanpro <corbanpro@gmail.com>
2023-11-20 18:38:58 -0700

    allow settings to read and write user data

--- src/Settings.js
+++ src/Settings.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useContext } from "react";
+import { UserContext } from "./UserContext";
 
-const Settings = () => {
-    return (
-        <div>
-            <h1>Settings</h1>
-        </div>
-    );
-}
+const Settings = ({ user, setUser }) => {
+  const userContext = useContext(UserContext);
+  const switchUserHandler = () => {
+    if (user.id === 0) {
+      setUser(userContext.users[1]);
+    } else {
+      setUser(userContext.users[0]);
+    }
+  };
+  return (
+    <div className="p-2">
+      <h1>Settings</h1>
+      <h2>
+        Hi {user.sex === "m" ? "Elder" : "Sister"} {user.lastName}
+      </h2>
+      <button
+        className="btn text-white bg-primary mb-5 me-1 position-absolute bottom-0 end-0"
+        onClick={switchUserHandler}
+      >
+        Switch User
+      </button>
+    </div>
+  );
+};
 
 export default Settings;
