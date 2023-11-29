ac78dd4a931a28e496f7a7d94135368827e29849
corbanpro <corbanpro@gmail.com>
2023-11-28 21:37:01 -0700

    grid page started (looks bad)

--- src/Grid.js
+++ src/Grid.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React from "react";
 
-const Grid = () => {
-    return (
-        <div>
-            <h1>Grid by Sawyer...</h1>
-        </div>
-    );
-}
+const Box = ({ day }) => {
+  const mood = day.mood;
+  let color = null;
+  if (mood === "happy") {
+    color = "yellow";
+  } else if (mood === "sad") {
+    color = "blue";
+  } else if (mood === "mad") {
+    color = "red";
+  } else {
+    color = "white";
+  }
+
+  return (
+    <div className="w-100 h-100" style={{ backgroundColor: color, height: 30 }}>
+      &nbsp;
+    </div>
+  );
+};
+
+const Grid = ({ user, setUser }) => {
+  return (
+    <div className="container">
+      <div>Your Mood Calendar</div>
+      <div className="grid-7">
+        {user.grid.days.map((day, i) => {
+          return <Box day={day} />;
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default Grid;
